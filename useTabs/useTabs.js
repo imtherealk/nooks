ef57c1ee8f4d247bfddc6c560e623c45cf893bbe
Nayeong <imtherealk@gmail.com>
2020-12-22 21:25:53 +0900

    Add useTabs

---
+++ useTabs/useTabs.js
@@ -0,0 +1,11 @@
+export const useTabs = (initialTab, allTabs) => {
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+  }
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex,
+  };
+};
