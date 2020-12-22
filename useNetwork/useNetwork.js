886608610a8b8c2e379b86e5b482e10e7a897159
Nayeong <imtherealk@gmail.com>
2020-12-23 00:58:04 +0900

    Add useNetwork

---
+++ useNetwork/useNetwork.js
@@ -0,0 +1,20 @@
+const useNetwork = (onChange) => {
+  const [status, setStatus] = useState(navigator.onLine);
+
+  const handleChange = () => {
+    if (onChange && typeof onChange === "function") {
+      onChange(navigator.onLine);
+    }
+    setStatus(navigator.onLine);
+  };
+  useEffect(() => {
+    window.addEventListener("online", handleChange);
+    window.addEventListener("offline", handleChange);
+
+    return () => {
+      window.removeEventListener("online", handleChange);
+      window.removeEventListener("offline", handleChange);
+    };
+  }, []);
+  return { status };
+};
